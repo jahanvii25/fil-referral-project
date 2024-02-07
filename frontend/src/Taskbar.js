9c6c0ae14dd991e27cf6a20044f29e85712cee8e
priyumm <priyamdaspatnaik@gmail.com>
2024-02-07 16:14:41 +0530

    getting started with frontend

---
+++ frontend/src/Taskbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Taskbar.css';
+
+function Taskbar() {
+  return (
+    <div className="taskbar">
+      <img src="logo.png" alt="Logo" className="logo" />
+    </div>
+  );
+}
+
+export default Taskbar;
